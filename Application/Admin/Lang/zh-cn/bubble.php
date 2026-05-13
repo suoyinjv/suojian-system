d5f416c841a6773934cecb6be55abe582c963c6a
suojianqiang <15199b9fc4ac176a69d01dc0fddc6efc>
2026-05-13 13:48:18 +0800

    fix: 冒泡广场视图全部转原生PHP，修复XML标签语法错误

--- Application/Admin/Lang/zh-cn/bubble.php
+++ Application/Admin/Lang/zh-cn/bubble.php
@@ -11,5 +11,9 @@ return array(
 	'bubble_praise_table_nickname'				=>	'用户',
 	'bubble_praise_table_add_time'				=>	'点赞时间',
 	'bubble_so_keyword_tips'					=>	'昵称/手机/邮箱/说说',
+	'bubble_mood_praise_error'					=>	'不能点赞自己的说说',
+	'bubble_mood_comments_error'				=>	'不能评论自己的说说',
+	'bubble_comments_placeholder'				=>	'评论内容',
+	'bubble_comments_format'					=>	'先随便说两句吧......',
 );
 ?>
